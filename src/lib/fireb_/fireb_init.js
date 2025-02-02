0119848f5ec514441603451227647d4b544fcf72
John Henry Thompson <jht9629@nyu.edu>
2025-02-02 16:59:48 -0500

    publish

--- src/lib/fireb_/fireb_init.js
+++ src/lib/fireb_/fireb_init.js
@@ -74,7 +74,7 @@ function select_config(config) {
   // if config object not found, default to firebaseConfig_jht9629
   nconfig = nconfig || firebaseConfig_jht9629;
   nconfig.configLabel = configLabel;
-  nconfig.configVersion = '?v=58';
+  nconfig.configVersion = '?v=59';
   // console.log('fireb_config config', config);
   // console.log('fireb_config config.projectId', config.projectId);
 
