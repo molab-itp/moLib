77122ba3f6aa6d0144f3817eb56aeeae701788f8
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-07 11:18:04 -0500

    downloadToFile

--- src/lib/RefBox.js
+++ src/lib/RefBox.js
@@ -1,3 +1,5 @@
+//
+
 export class RefBox {
   //
   //  storageLabel: 'refBox1',
@@ -161,3 +163,19 @@ export class RefBox {
 // }
 
 globalThis.RefBox = RefBox;
+
+// https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
+//
+export function downloadToFile(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+globalThis.downloadToFile = downloadToFile;
